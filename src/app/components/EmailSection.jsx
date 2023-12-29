6ba402da8ddcd1be814e7e0e7a5ef2b261658150
Ahsan <sbaseer25@gmail.com>
2023-12-28 23:44:11 -0500

    minor changes

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 
 const EmailSection = () => {
   const [emailSubmitted, setEmailSubmitted] = useState(false);
+  const emailAddress = "sbaseer25@gmail.com";
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -51,15 +52,22 @@ const EmailSection = () => {
         </h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
-          I&apos;m currently looking for new opportunities, my inbox is always
+          I&apos;m currently looking for new internship opportunities for Spring and Fall 2024, my inbox is always
           open. Whether you have a question or just want to say hi, I&apos;ll
-          try my best to get back to you!
+          try my best to get back to you! 
+          <br></br>
+          <br></br>
+          
+          Email me at:{" "}
+          <a href={`mailto:${emailAddress}`} className="text-primary-500">
+          {emailAddress}
+        </a>
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="https://github.com/Ahsan725">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com">
+          <Link href="https://www.linkedin.com/in/ahsan-/">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
@@ -84,7 +92,7 @@ const EmailSection = () => {
                 id="email"
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="jacob@google.com"
+                placeholder="example@google.com"
               />
             </div>
             <div className="mb-6">
@@ -100,7 +108,7 @@ const EmailSection = () => {
                 id="subject"
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="Just saying hi"
+                placeholder="Opportunities /  Questions"
               />
             </div>
             <div className="mb-6">
@@ -114,7 +122,7 @@ const EmailSection = () => {
                 name="message"
                 id="message"
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="Let's talk about..."
+                placeholder="Let's chat about..."
               />
             </div>
             <button
